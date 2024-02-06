486b30fbdaf64bfcbabb3098389815f285f4f49b
MarioB06 <mbozic2006@gmail.com>
2024-02-06 14:58:07 +0100

    dahsboard aussehen

--- soccerBet/database/migrations/2014_10_12_000000_create_users_table.php
+++ soccerBet/database/migrations/2014_10_12_000000_create_users_table.php
@@ -17,6 +17,7 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->decimal('account', 10, 2)->default(3.00);
             $table->rememberToken();
             $table->timestamps();
         });
